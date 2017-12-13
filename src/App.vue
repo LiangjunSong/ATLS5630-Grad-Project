4de27c9dfcc0f686da4d6b8541ef89b7d24ecbda
Lich_Amnesia <alwaysxiaop@gmail.com>
2017-12-12 19:59:55 -0700

    add three.js

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@
       v-for="comp in components"
       :songsPar="songsPar"
     />
+      <div id="cube" style="width:400px; height:400px;">THREE.JS should appear here</div>
+        <div id="cubeapp">
+
+        </div>
   </div>
 </template>
 
@@ -16,6 +20,7 @@
 import Search from './components/Search'
 import Playlist from './components/Playlist'
 import axios from 'axios';
+import * as THREE from 'three';
 
 export default {
   name: 'app',
@@ -36,6 +41,39 @@ export default {
           components: 1,
           index: 1
       }
+  },
+  mounted() {
+      console.log("mounted");
+      var scene = new THREE.Scene();
+      var camera = new THREE.PerspectiveCamera( 75, 400 / 400, 0.1, 1000 );
+
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setSize( 400, 400 );
+      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+      var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe:true } );
+      var cube = new THREE.Mesh( geometry, material );
+      scene.add( cube );
+
+      camera.position.z = 5;
+
+      document.getElementById("cube").appendChild( renderer.domElement );
+      function render() {
+        requestAnimationFrame( render );
+        renderer.render( scene, camera );
+        cube.rotation.x += 0.01;
+          cube.rotation.y += 0.01;
+      }
+      render();
+      /*
+
+      ThreeJs custom waves
+      Original script by ThreeJS : https://threejs.org/examples/canvas_particles_waves.html
+      Modified version for Cloudoru by Kevin Rajaram : http://kevinrajaram.com
+      Date: 08/14/2014
+
+
+      */
+
   },
   methods: {
     getPlayList(tags) {
--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
